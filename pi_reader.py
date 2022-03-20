adeca38877f7101baf4f213050a1fb8485fb0ada
nsandlin1 <novi@h8n.com>
2022-03-20 18:24:34 -0500

    initial sets

---
+++ pi_reader.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+INTERVAL = 1
+
+# Set pinmode layout
+GPIO.setmode(GPIO.BCM)
+
+def get_temp():
+    ...
+
+if __name__ == "__main__": 
+    get_temp()
+
