f6a7ec6a2464c19908df088ae34b6d7713a6c312
nsandlin1 <novi@h8n.com>
2022-04-02 19:42:56 -0500

    stuff

--- getinfo.py
+++ getinfo.py
@@ -1,18 +1,33 @@
-import adafruit_dht
+import Adafruit_DHT
 import board
 import time
+from prometheus_client import start_http_server, Gauge, Summary
+
+collection_interval = 5
+
+temperature = Gauge("temperature_data", "temperature data gathered by DHT22")
+humidity = Gauge("humdity_data", "humidity data gathered by DHT22")
 
 # initialize dht device without pulse
-thermometer = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+thermometer = Adafruit_DHT.DHT22(board.D4, use_pulseio=False)
 
-while True:
-    try: 
-        temp_F = str(thermometer.temperature * (9/5) + 32)
-        humidity = str(thermometer.humidity)
+def get_temp_data():
+    while True:
+        try: 
+            temp_F = str(thermometer.temperature * (9/5) + 32)
+            humidity = str(thermometer.humidity)
 
-        print("Temperature: {:10s}  Humidity: {:10s}".format(temp_F, humidity))
+            print("Temperature: {:10s}  Humidity: {:10s}".format(temp_F, humidity))
 
-    except Exception:
-        raise Exception
+        except Exception:
+            raise Exception
 
-    time.sleep(2)
+        time.sleep(collection_interval)
+
+def main(): 
+    start_http_server(9111)
+    get_temp_data()
+
+if __name__ == '__main__':
+    main()
+
